ea7bf925cc6f9799ae740cc1186b8fc54eaaf2ca
Yoshiki YAMADA <nyamadandan@gmail.com>
2024-10-02 07:09:36 +0900

    ♻️  リファクタリング(parse.rs): 新しい関数を追加し、関数の分割と再利用性を向上させました これらの変更は、コードの可読性と保守性を向上させるために行われました。新しい関数parse_expression_statementとparse_expressionは、コードの再利用性を高めるために追加されました。また、関数parse_statementは複数のパーサー関数をループして処理するように変更され、コードの分割と整理が行われました。

--- src/parse.rs
+++ src/parse.rs
@@ -1,3 +1,5 @@
+use std::intrinsics::mir::ReturnTo;
+
 use crate::lex::{Token, TokenKind};
 
 #[derive(Debug)]
@@ -88,8 +90,164 @@ fn expect_identifier(tokens: &[Token], index: usize, value: &str) -> bool {
     t.kind == TokenKind::Identifier && t.value == value
 }
 
-pub fn parse(_raw: &[char], _tokens: Vec<Token>) -> Result<Ast, String> {
-    unimplemented!()
+fn parse_expression_statement(
+    raw: &[char],
+    tokens: &[Token],
+    index: usize,
+) -> Option<(Statement, usize)> {
+    let mut next_index = index;
+    let res = parse_expression(raw, tokens, next_index)?;
+
+    let (expr, next_next_index) = res;
+    next_index = next_next_index;
+    if !expect_syntax(tokens, index, ":") {
+        println!(
+            "{}",
+            tokens[next_index]
+                .loc
+                .debug(raw, "Expected semicolon after expression:")
+        );
+        return None;
+    }
+
+    next_index += 1;
+    Some((Statement::Expression(expr), next_index))
+}
+
+fn parse_expression(raw: &[char], tokens: &[Token], index: usize) -> Option<(Expression, usize)> {
+    if index >= tokens.len() {
+        return None;
+    }
+
+    let t = tokens[index].clone();
+    let left = match t.kind {
+        TokenKind::Number => Expression::Literal(Literal::Number(t)),
+        TokenKind::Identifier => Expression::Literal(Literal::Identifier(t)),
+        _ => {
+            return None;
+        }
+    };
+
+    let mut next_index = index + 1;
+    if expect_syntax(tokens, next_index, "(") {
+        // skip past open paran
+        next_index += 1;
+
+        // function call
+        let mut arguments: Vec<Expression> = vec![];
+
+        while !expect_syntax(tokens, next_index, ")") {
+            if arguments.is_empty() {
+                if !expect_syntax(tokens, next_index, ",") {
+                    println!(
+                        "{}",
+                        tokens[next_index]
+                            .loc
+                            .debug(raw, "Expected comma between function call arguments:")
+                    );
+                    return None;
+                }
+                next_index += 1;
+            }
+
+            let res = parse_expression(raw, tokens, next_index);
+            if let Some((arg, next_next_index)) = res {
+                next_index = next_next_index;
+                arguments.push(arg);
+            } else {
+                println!(
+                    "{}",
+                    tokens[next_index]
+                        .loc
+                        .debug(raw, "Expected valid expression in function call arguments:")
+                );
+                return None;
+            }
+        }
+
+        next_index += 1; //skip past closing paren
+
+        return Some((
+            Expression::FunctionCall(FunctionCall {
+                name: tokens[index].clone(),
+                arguments,
+            }),
+            next_index,
+        ));
+    }
+
+    // Might be a literal expression
+    if next_index >= tokens.len() || tokens[next_index].clone().kind != TokenKind::Operator {
+        return Some((left, next_index));
+    }
+
+    // Otherwise is a binary operation
+    let op = tokens[next_index].clone();
+    next_index += 1; // Skip past op
+
+    if next_index >= tokens.len() {
+        println!(
+            "{}",
+            tokens[next_index]
+                .loc
+                .debug(raw, "Expected valid tight hand side binary perand:")
+        );
+        return None;
+    }
+
+    let rtoken = tokens[next_index].clone();
+    let right = match rtoken.kind {
+        TokenKind::Number => Expression::Literal(Literal::Number(rtoken))
+        TokenKind::Identifier => Expression::Literal(Literal::Identifier(rtoken))
+        _ => {
+            println!("{}", rtoken.loc.debug(raw, "Expected valid right hand side binary operand:"));
+            return None;
+        }
+    };
+
+    next_index += 1;
+
+    Some((Expression::BinaryOperation(BinaryOperation {
+        left: Box::new(left),
+        right: Box::new(right),
+        operator: op,
+    }), next_index))
+}
+
+fn parse_statement(raw: &[char], tokens: &[Token], index: usize) -> Option<(Statement, usize)> {
+    let parsers = [
+        parse_if,
+        parse_expression_statement,
+        parse_return,
+        parse_local,
+    ];
+
+    for parser in parsers {
+        let res = parser(raw, tokens, index);
+        if res.is_some() {
+            return res;
+        }
+    }
+
+    None
+}
+
+pub fn parse(raw: &[char], tokens: Vec<Token>) -> Result<Ast, String> {
+    let mut ast = vec![];
+    let mut index = 0;
+    let ntokens = tokens.len();
+    while index < ntokens {
+        let res = parse_statement(raw, &tokens, index);
+        if let Some((stmt, next_index)) = res {
+            index = next_index;
+            ast.push(stmt);
+            continue;
+        }
+
+        return Err(tokens[index].loc.debug(raw, "Invalid token while parsing:"));
+    }
+
+    Ok(ast)
 }
 
 #[cfg(test)]
